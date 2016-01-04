ee30ad2652e3a641e4603d1118d1e73c52d088ef
Jasper Koops <jasperkoops91@gmail.com>
2016-01-04 22:21:26 +0100

    Programma gaat naar de beyond!
    - Prijs bereken functie ingevoegd
    - Ingredienten + prijs ingevoegd (prijs moet meestal nog bepaald worden
    - Programma kan nu per recept de ingredienten uitlezen en de prijs bepalen
    - ^Deze functie moet naar de the_cake, staat verder niet in de main loop

--- recepten.py
+++ recepten.py
@@ -1,10 +1,17 @@
 food_opties = {
-        "Ultra bonen rijst": ["100 gram rijst", "1 tomaat", "1 ui", "capucijners", "spek_reepjes"],
-        "Vega couscous": ["100 gram couscous", "wortel", "geroosterde paprika", "cashewnoten", "prei"],
-        "Ultra linzen rijst": ["100 gram rijst", "1 tomaat", "1 ui", "linzen", "spekreepjes"],
-        "Libanese Linzen": ["100 gram rijst", "Yoghurt", "100 gram linzen", "limoensap", "rode uien", "kruiden"],
-        "Mocro couscous": ["100 gram couscous", "100 gram linzen", "Yoghurt", "limoensap", "ui"],
+        "ultra bonen rijst": ["rijst", "tomaat", "ui", "capucijners", "spekreepjes"],
+        "vega couscous": ["couscous", "wortel", "geroosterde paprika", "cashewnoten", "prei"],
+        "ultra linzen rijst": ["rijst", "tomaat", "ui", "linzen", "spekreepjes"],
+        "libanese linzen": ["rijst", "yoghurt", "linzen", "limoensap", "rode uien", "kruiden"],
+        "mocro couscous": ["couscous", "linzen", "yoghurt", "limoensap", "ui"],
         }
+
+test = {
+
+        "couscous": ["couscous", "wortel", "prei"],
+        "rijst met kip": ["rijst", "prei", "kip"]
+        }
+
 
 
 """
@@ -19,3 +26,51 @@ Prei: 0.40
 
 
 """
+
+ingredienten = {
+    #MAAAK HEM ALFABETISCH
+
+    # ^ staat hij volgens mij
+
+    "capucijners": 0.00, #!!!!!!
+    "cashewnoten": 0.00, # !!!!!
+    "couscous": 0.00, #!!!!!
+    "geroosterde paprika": 0.00, #!!!!!
+    "kip": 0.00, # !!!!!!!
+    "limoensap": 0.00, # !!!!!!
+    "linzen": 0.00, # !!!!!!!
+    "prei": 0.40,
+    "rijst":0.00, # !!!!!!
+    "rode ui": 0.00, #!!!!!!!
+    "spekreepjes": 0.00, # !!!!!
+    "tomaat": 0.00, # !!!!!
+    "ui": 0.00, #!!!!!!!!!!
+    "wortel": 0.18,
+    "yoghurt": 0.00, # !!!!!
+
+
+
+}
+
+
+recepten = {
+
+    "Mocro couscous" : ["couscous", "wortel", "prei"]
+}
+
+def prijs(recept):
+    """Rekent de prijs uit van een recept """
+    prijs = 0
+    for gerecht in food_opties[recept]:
+        for key, value in ingredienten.items():
+            if key == gerecht:
+                prijs += value
+    return prijs
+
+# ^zoiets?
+
+
+
+print prijs("couscous")
+
+print prijs("rijst met kip")
