cbfe8a54089cdadaac397a9ba9030f1848403a20
Jasper Koops <jasperkoops91@gmail.com>
2016-03-27 01:23:46 +0100

    BESTAANDE RECEPTEN UITGESCHREVEN EN NIEUWE RECEPTEN TOEGEVOEGD
    - De prijzenlijst slaat nog steeds nergens op
    - Het aantal recepten is met 10 nog erg beperkt.

--- recepten.py
+++ recepten.py
@@ -2,25 +2,9 @@ import random
 
 # GEEN CAPS GEBRUIKEN!!!
 
-
-
-
-"""
-Rijst gerechten:
-Rijs: 0.28 (12.50 / 45)
-
-Vega Couscous:
-
-couscous: 0.23
-wortel: 0.18
-Prei: 0.40
-
-
-"""
-
 food_opties = {
 
-        #RECEPT
+        #RECEPT 1
         "ultra bonen rijst": ["""
 
         1. Zet een pan op het vuur en breng het water aan de kook
@@ -36,7 +20,7 @@ food_opties = {
         ["rijst", "tomaat", "ui", "capucijners", "spekreepjes"]],
 
 
-        #RECEPT
+        #RECEPT 2
         "vega couscous": ["""
         1. Snij de prei en doe deze in de pan
         2. Snij de wortel en voeg deze vrij snel toe (om te voorkomen dat hij hard blijft)
@@ -47,7 +31,7 @@ food_opties = {
        ["Boullion blokje, couscous", "wortel", "geroosterde paprika", "cashewnoten", "prei", "kruiden"]],
 
 
-        #RECEPT
+        #RECEPT 3
         "ultra linzen rijst": ["""
 
         1. Zet een pan op het vuur en breng het water aan de kook
@@ -63,7 +47,7 @@ food_opties = {
         ["rijst", "tomaat", "ui", "linzen blik", "spekreepjes"]],
 
 
-        #RECEPT
+        #RECEPT 4
         "libanese linzen": ["""
 
         1. Snij de ui in blokjes en bak ze totdat ze karameliseren (of als dat niet kan, want ik ben dit recept vergeten.), ze de helft appart.
@@ -76,18 +60,20 @@ food_opties = {
         ["rijst", "yoghurt", "linzen", "limoensap", "rode uien", "kruiden"]],
 
 
-        #RECEPT
+        #RECEPT 5
         "mocro couscous": ["""
 
-        mocro_couscous_recept
-        mocro_couscous_recept
-        mocro_couscous_recept
-        mocro_couscous_recept
+        1. Verwarm olie in een pan.
+        2. Snijd ondertussen de ui en knoflook klein.
+        3. Rooster de groenten en voeg de helft van de kruiden toe. Roer goed en wacht tot de geur loskomt.
+        4. Voeg de couscous, linzen en een klein beetje water toe (KLEIN BEETJE, het moet geen soep worden - houd het droog) gooi de rest van de kruiden, limoensap en zout erbij en roer een paar minuten goed.
+        5. Serveer met yoghurt.
+        6. Probeer te genieten. Geen idee of ik dit chill vond toen ik dit maakte.
         """,
-        ["couscous", "linzen blik", "yoghurt", "limoensap", "ui", "kruiden"]],
+        ["couscous", "linzen blik", "yoghurt", "limoensap", "ui", "kruiden", "knoflook"]],
 
 
-        #RECEPT
+        #RECEPT 6
         "nice nasi": ["""
 
         1. Breng het water aan de kook
@@ -102,7 +88,7 @@ food_opties = {
         ["rijst", "ui", "paprika", "wortel", "kruiden", "cashewnoten" ]],
 
 
-        #RECEPT
+        #RECEPT 7
         "standaard pasta": ["""
 
         1. Breng het water aan de kook
@@ -115,31 +101,62 @@ food_opties = {
         """,
         ["pasta", "ui", "1 blik gepelde tomaat", "kaas", "knoflook", "basalmico azijn" ]],
 
-        #RECEPT
+        #RECEPT 8
         "ULTRA WOK": ["""
 
-        1. Zet een grote wok op het hoogste vuur ooit, op max en VERDAMP de olie.
-
-        1. Snijd alle groente, de broccoli, de paprika en de ui.
-        2. Kook de rijst en giet deze af.
-
-
-
-
-        ]
-
-
-        }
-
-
+        1. Breng in een pan water (met wat zout) aan de kook voor de rijst.
+        2. Zet een grote wok op het hoogste vuur ooit, op max en VERDAMP de olie. Probeer temperaturen te bereiken waarin je de one ring zou kunnen smelten.
+        3. Doe, als het water in pan 1 kookt, de rijst erin en kook deze.
+        4. Snijd ondertussen alle groente (de brocolli, paprika en ui)
+        5. Giet de rijst af en zet hem apart.
+        6. Doe de groente in de wok en bak 1 minuut, GOED roeren. Shit brand aan yo
+        7. Voeg de woksaus toe (niet teveel! In zakjes de helft doen)
+        8. Voeg de rijst toe, goed roeren!
+        9. Voeg de kruiden en cashewnoten toe.
+        10. Serveer, eet, ervaar, geniet.
+        """,
+        ["rijst", "ui", "paprika", "brocolli", "cashewnoten", "woksaus", "kruiden"]],
 
 
+        #RECEPT 9
+        "BONKERS BURGER": ["""
+
+        1. Verwarm olie in 2 pannen.
+        2. Vermeng 1 eetlepel mayo, 1 eetlepel ketchup, een theelepel mosterd en een paar druppels azijn in een bakje. Dit is de saus.
+        3. Doe de aardappelschijfjes in pan 1 en bak deze.
+        4. Snijd ondertussen de ui in ringen.
+        5. Snijd de broodjes open
+        6. Bak de burgers in pan 2 - eventueel kruiden.
+        7. Doe 1 minuut voordat de burgers gaar zijn de uien in de pan en bak deze kort mee.
+        8. Leg ondertussen snel de plakjes kaas op de burgers zelf.
+        9. Bak de broodjes kort mee in de vleespan.
+        10. Smeer de bovenstte binnenkant van de broodjes in met saus. Leg de burger op de onderste binnenkant. Leg ui en augurk op de burger. Kruid eventueel.
+        11. Serveer, eet, ervaar, geniet.
+        """,
+        ["broodjes", "hamburgers", "kaas", "ui", "augurk", "mayonaisse", "ketchup", "mosterd", "basalmico azijn", "aardappelschijfjes"]],
+
+        #RECEPT 10
+        "FANCY DAHL": ["""
+
+        1. Breng een pan met water en zout aan de kook voor de rijst.
+        2. Zet de sauspan, ZONDER OLIE, op het vuur en rooster daarin de amandelschijfjes. PAS OP: dit gaat op een gevenmoment motherfucking snel yo.
+        3. Snijd ondertussen de wortel, ui en tomaten klein.
+        4. Verwarm olie in een sauspan, en voeg als deze heet genoeg is de ui, tomaat en wortel toe.
+        5. Kruid bij met wat koriander.
+        6. Kook ondertussen de rijst en zet deze appart als hij klaar is.
+        7. Ga ondertussen die hard door met de dahl, gooi de linzen bij de groenten als deze een prut is. Roer goed en bak 1 minuut
+        8. Voeg de kokosmelk toe, het moet 1 vinger dikte boven de linzen uitkomen. 200Ml should do it, zo'n blik is overkill. Voeg eventueel wat water toe.
+        9. Breng het geheel aan de kook en laat het zachtjes pruttelen, vergeet niet af en toe te roeren want anders bakt het aan.
+        10. Als het een soort van pap wordt dan is dat chill. Voeg wat limoensap, zout, peper en de rest van de kruiden toe
+        11. Roer goed.
+        12. Serveer met rijst en strooi de geroosterde amandelschijfjes erover heen.
+        13. Dit is als het goed is het beste wat je ooit hebt gegeten. Als het dat niet is dan ben je persoonlijk tekort geschoten.
+        """,
+
+        ["rijst", "linzen rood", "ui", "tomaat", "wortel", "kokosmelk mini", "kruiden", "limoensap", "amandelschijfjes"]],
 
 
 
-
-
-        ]
 
 
 
@@ -149,9 +166,14 @@ ingredienten = {
     #MAAAK HEM ALFABETISCH
 
     # ^ staat hij volgens mij
+    #^^^^^^^^NOPE NIET MEER.
+    #^ DIT DING MOET IN EEN APPART BESTAND GAAN
 
     #MEESTE PRIJZEN ZIJN NEP
     "aardappel": 0.50, # !!!!!!!
+    "aardappelschijfjes": 0.80, #!!!!!!!!!!!!!!!!
+    "amandelschijfjes": 0.20, #!!!!!!!!!!!!!!!!!
+    "augurk": 0.05, #!!!!!!!!!
     "basalmico azijn": 0.30, # !!!!!!!!!!!!
     "blik gepelde tomaat": 0.52,
     "broccoli": 0.70,
@@ -163,8 +185,10 @@ ingredienten = {
     "kip": 4.00, # !!!!!!!
     "knoflook": 0.10, # !!!!!!!!!!!!
     "kroepoek": 0.43,
+    "kokosmelk mini": 1.20, #!!!!!!!!!!!!!!!!
     "limoensap": 0.20, # !!!!!!
-    "linzen": 0.30, # !!!!!!!
+    "linzen groen": 0.30, # !!!!!!!
+    "linzen rood": 0.20, #!!!!!!!!!
     "linzen blik": 0.80, # !!!!!!
     "paprika":0.46,
     "pasta": 0.20, # !!!!!!!!!!
