2f0fc74a884085a3ca7023375f96df6d84e8be77
Jasper Koops <jasperkoops91@gmail.com>
2016-03-28 20:23:01 +0200

    Ingredienten ingevoegd als appart bestand

---
+++ ingredients.py
@@ -0,0 +1,41 @@
+
+ingredienten = {
+    #MAAAK HEM ALFABETISCH
+
+    # ^ staat hij volgens mij
+    #^^^^^^^^NOPE NIET MEER.
+    #^ DIT DING MOET IN EEN APPART BESTAND GAAN
+
+    #MEESTE PRIJZEN ZIJN NEP
+    "aardappel": 0.50, # !!!!!!!
+    "aardappelschijfjes": 0.80, #!!!!!!!!!!!!!!!!
+    "amandelschijfjes": 0.20, #!!!!!!!!!!!!!!!!!
+    "augurk": 0.05, #!!!!!!!!!
+    "basalmico azijn": 0.30, # !!!!!!!!!!!!
+    "blik gepelde tomaat": 0.52,
+    "broccoli": 0.70,
+    "capucijners": 0.83,
+    "cashewnoten": 0.51, #!!!!!!!!!!!!!!!!!
+    "couscous": 0.20, #!!!!!
+    "geroosterde paprika": 0.80, #!!!!!
+    "kaas": 0.20, # !!!!!!!!!!
+    "kip": 4.00, # !!!!!!!
+    "knoflook": 0.10, # !!!!!!!!!!!!
+    "kroepoek": 0.43,
+    "kokosmelk mini": 1.20, #!!!!!!!!!!!!!!!!
+    "limoensap": 0.20, # !!!!!!
+    "linzen groen": 0.30, # !!!!!!!
+    "linzen rood": 0.20, #!!!!!!!!!
+    "linzen blik": 0.80, # !!!!!!
+    "paprika":0.46,
+    "pasta": 0.20, # !!!!!!!!!!
+    "prei": 0.60,
+    "rijst":0.20, # !!!!!!
+    "rode ui": 0.24, #!!!!!!!
+    "spekreepjes": 0.85, # !!!!!
+    "tomaat": 0.50,
+    "ui": 0.11, #!!!!!!!!!!
+    "wortel": 0.16,
+    "yoghurt": 0.80, # !!!!!
+
+}
--- recepten.py
+++ recepten.py
@@ -1,6 +1,22 @@
 import random
 
+"""
+Dit is een dictionary met als value de recepten namen en als waarden een grot elijst met daarin een string (het recept) en een lijst van ingredienten
+
+food_opties[0] =
+
+
+"""
+
 # GEEN CAPS GEBRUIKEN!!!
+
+#PULAO! (of hoe dat ook heet)
+#Pasta recepten
+#Bami varianten?
+#Die ene BBC kokos annas soep
+#BOvenstaande soep met kip
+
+
 
 food_opties = {
 
@@ -28,7 +44,7 @@ food_opties = {
         4. Voeg tegen het einde de kruiden en de geroosterde paprika toe en roer goed.
         5. Voeg de cashewnoten toe en server.
         """,
-       ["Boullion blokje, couscous", "wortel", "geroosterde paprika", "cashewnoten", "prei", "kruiden"]],
+       ["Boullion blokje", "couscous", "wortel", "geroosterde paprika", "cashewnoten", "prei", "kruiden"]],
 
 
         #RECEPT 3
@@ -135,6 +151,7 @@ food_opties = {
         """,
         ["broodjes", "hamburgers", "kaas", "ui", "augurk", "mayonaisse", "ketchup", "mosterd", "basalmico azijn", "aardappelschijfjes"]],
 
+
         #RECEPT 10
         "FANCY DAHL": ["""
 
@@ -155,50 +172,4 @@ food_opties = {
 
         ["rijst", "linzen rood", "ui", "tomaat", "wortel", "kokosmelk mini", "kruiden", "limoensap", "amandelschijfjes"]],
 
-
-
-
-
-
-
-
-ingredienten = {
-    #MAAAK HEM ALFABETISCH
-
-    # ^ staat hij volgens mij
-    #^^^^^^^^NOPE NIET MEER.
-    #^ DIT DING MOET IN EEN APPART BESTAND GAAN
-
-    #MEESTE PRIJZEN ZIJN NEP
-    "aardappel": 0.50, # !!!!!!!
-    "aardappelschijfjes": 0.80, #!!!!!!!!!!!!!!!!
-    "amandelschijfjes": 0.20, #!!!!!!!!!!!!!!!!!
-    "augurk": 0.05, #!!!!!!!!!
-    "basalmico azijn": 0.30, # !!!!!!!!!!!!
-    "blik gepelde tomaat": 0.52,
-    "broccoli": 0.70,
-    "capucijners": 0.83,
-    "cashewnoten": 0.51, #!!!!!!!!!!!!!!!!!
-    "couscous": 0.20, #!!!!!
-    "geroosterde paprika": 0.80, #!!!!!
-    "kaas": 0.20, # !!!!!!!!!!
-    "kip": 4.00, # !!!!!!!
-    "knoflook": 0.10, # !!!!!!!!!!!!
-    "kroepoek": 0.43,
-    "kokosmelk mini": 1.20, #!!!!!!!!!!!!!!!!
-    "limoensap": 0.20, # !!!!!!
-    "linzen groen": 0.30, # !!!!!!!
-    "linzen rood": 0.20, #!!!!!!!!!
-    "linzen blik": 0.80, # !!!!!!
-    "paprika":0.46,
-    "pasta": 0.20, # !!!!!!!!!!
-    "prei": 0.60,
-    "rijst":0.20, # !!!!!!
-    "rode ui": 0.24, #!!!!!!!
-    "spekreepjes": 0.85, # !!!!!
-    "tomaat": 0.50,
-    "ui": 0.11, #!!!!!!!!!!
-    "wortel": 0.16,
-    "yoghurt": 0.80, # !!!!!
-
-}
+        }
